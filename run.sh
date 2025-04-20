1f867e449b00df49e9b7bc1f5fb72f6be5f21dbb
doitomkar123 <doitomkar123@gmail.com>
2025-04-20 13:43:29 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ then
   kill  -9 $(ps -ef | grep target/demo-0.0.1-SNAPSHOT.jar | grep -v grep)
 fi
 
-if nohup java -jar -Dserver.port=80 target/demo-0.0.1-SNAPSHOT.jar &
+if nohup java -jar -Dserver.port=8081 target/demo-0.0.1-SNAPSHOT.jar &
 then
   echo "Deployed New Application"
 fi
