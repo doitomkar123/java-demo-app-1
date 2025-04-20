87cb8a3d7640dc3b1be6ee5f7f7f04c91b32bd25
doitomkar123 <doitomkar123@gmail.com>
2025-04-20 13:30:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 if ps -ef | grep target/demo-0.0.1-SNAPSHOT.jar | grep -v grep
 then
   echo "Killing existing process"
+  kill  -9 $(ps -ef | grep target/demo-0.0.1-SNAPSHOT.jar | grep -v grep)
 fi
 
 if nohup java -jar -Dserver.port=80 target/demo-0.0.1-SNAPSHOT.jar &
